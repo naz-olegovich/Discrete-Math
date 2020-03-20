7017370290573697ff67e64db1d3fc6629fec3b6
Nazar Chopyk <61637619+naz-olegovich@users.noreply.github.com>
2020-03-20 12:13:09 +0200

    Final code

--- lab2(remastered)/Relation_R.py
+++ lab2(remastered)/Relation_R.py
@@ -1,24 +1,12 @@
 from random import randint, sample, shuffle
 
-def relations_R(A, B, list_of_women_names, list_of_men_names, S):
+
+def relations_R(A, B, list_of_women_names, list_of_men_names):
     relations_R_list = list()
     a = list()
     b = list()
-    #################################################################################################
-    list_dad_with_daughter = list(filter(lambda daughter: daughter[1] in list_of_women_names, S))
-    list_dad_with_sun = list(filter(lambda sun: sun[1] in list_of_men_names, S))
-    list_of_women_dads = list()  # список бать жінок
-    list_of_men_dads = list()  # список бать чоловіків
-    list_of_t = list()  # список  тестів
-    list_of_z = list()  # список зятів
-
-    for i in range(len(list_dad_with_daughter)):
-        list_of_women_dads.append(list_dad_with_daughter[i][0])
-
-    for i in range(len(list_dad_with_sun)):
-        list_of_men_dads.append(list_dad_with_sun[i][0])
-
-
+    list_of_z = list()
+    list_of_t = list()
 
     for i in A:
         if i in list_of_men_names:
@@ -27,31 +15,15 @@ def relations_R(A, B, list_of_women_names, list_of_men_names, S):
     for j in B:
         if j in list_of_men_names:
             b.append(j)
-    #################################################################################################
 
-    for n in sample(a,len(a)):  # n-тесть  m-зать
-
-
-        #rndt = randint(1, 2)  # рандомна кількість зятів в тестя
-
-        for m in sample(b,len(b)):
-            if (n in list_of_men_dads):  # тесть не може бути батьком чоловіка( заберає багато суперечностей)
-                for i in list_dad_with_sun:
-                    if i[1] == m:
-                        continue
-
-            if list_of_women_dads.count(n) <= list_of_t.count(n):  # у тестя стільки зятів, скільки в нього дочок, може менше, але не більше
-                continue
+    for n in sample(a, len(a)):  # n-тесть  m-зать
+        num_of_z = randint(1, 2)
+        for m in sample(b, len(b)):
 
             if m in list_of_z:  # в зятя один тесть
                 continue
-
-            if m in list_of_women_dads:  # зять не може бути синоим тестю
+            if n in list_of_t:
                 continue
-
-            # if int(list_of_t.count(n)) >= rndt:  # рандомна кількість зятів в тестя
-            #     continue
-
             if [n, m] in relations_R_list:  # перевірки вже пара не входть в список
                 continue
             if n == m:  # тесть не може бути тестем сам собі
@@ -59,10 +31,10 @@ def relations_R(A, B, list_of_women_names, list_of_men_names, S):
             if m in A:
                 continue
 
-
             list_of_t.append(n)
             list_of_z.append(m)
             relations_R_list.append([n, m])
+
     print('Relation R')
     print(relations_R_list)
     return relations_R_list
