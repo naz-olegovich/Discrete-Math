fc174786ab6132b490de8d017feafea7298e92e7
Nazar Chopyk <61637619+naz-olegovich@users.noreply.github.com>
2020-03-20 11:26:36 +0200

    Start

---
+++ lab2(remastered)/Relation_S.py
@@ -0,0 +1,23 @@
+from random import randint, sample
+
+def relations_S(A, B, list_of_men_names):
+    relations_S_list = list()
+
+    for a in sample(A, randint(len(A) // 2, len(A))):  # min half elements of set will be considered
+        num_of_children = randint(1, 2)
+        for b in sample(B, randint(len(B) // 2, len(B))):
+            if a in list_of_men_names:
+                x = len(list(filter(lambda R: R[0] == a, relations_S_list)))
+                if list(filter(lambda R: R[1] == b, relations_S_list)):
+                    continue
+                if x >= num_of_children:
+                    continue
+                if b in A:
+                    continue
+
+                relations_S_list.append([a, b])
+    print('\nRalation S')
+    print(relations_S_list)
+
+
+    return relations_S_list
