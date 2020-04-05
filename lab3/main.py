1d9120993f7a11809200f64d5044b45bc3c65a24
Nazar Chopyk <61637619+naz-olegovich@users.noreply.github.com>
2020-04-05 13:38:59 +0300

    Created win2 (Adjacency matrix)

--- lab3/main.py
+++ lab3/main.py
@@ -6,12 +6,15 @@ import tkinter.messagebox
 import webbrowser
 
 
+
 class Window1:
     A = set()
     B = set()
     R = list()
     S = list()
-
+    bg = '#002451'
+    fr_bg = '#024FBF'
+    fg = '#A9B7C6'
     def __init__(self, main):
         self.main = root
 
@@ -19,7 +22,7 @@ class Window1:
         main_menu = Menu(root)
         elements_of_menu = Menu(main_menu, tearoff=0)
         main_menu.add_cascade(label="Windows", menu=elements_of_menu)
-        # elements_of_menu.add_command(label="Window 2", command=self.window2)
+        elements_of_menu.add_command(label="Window 2", command=self.window2)
         # elements_of_menu.add_command(label="Window 3", command=self.window3)
         # elements_of_menu.add_command(label="Window 4", command=self.window4)
         elements_of_menu.add_command(label="GitHub", command=lambda: webbrowser.open("https://github.com/naz-olegovich/Discrete-Math/tree/master/lab3"))
@@ -41,7 +44,7 @@ class Window1:
         # separated frame for entry and label
         frame_for_variant = Frame(text_frame, bg='#002451')
         frame_for_variant.pack()
-        number_in_list_label = Label(frame_for_variant, fg='#A9B7C6', font=("Ubuntu", 15), text="Номер у списку: 28", bg='#002451')
+        number_in_list_label = Label(frame_for_variant, fg='#A9B7C6', font=("Ubuntu", 15), text="Номер у списку: 28\nНомер залікової книжки: 9130", bg='#002451')
         number_in_list_label.grid(row=0, column=0)
 
 
@@ -53,6 +56,94 @@ class Window1:
         variant_button.grid(row=0, column=0)
         self.variant_label = Label(frame_for_variant2, font=30, bg='#002451', fg='#A9B7C6')
         self.variant_label.grid(row=0, column=1)
+
+
+    def window2(self):
+        def switch(event):
+            if event.widget.get() == "0":
+                event.widget.delete(0, END)
+                event.widget.insert(END, "1")
+            else:
+                event.widget.delete(0, END)
+                event.widget.insert(END, "0")
+
+        def build_graph():
+            plt.close()
+            plt.title("Матриця суміжності")
+            edges = list()
+            node_list1 = list()
+            node_list2 = list()
+            for i in list_of_entries:
+                for j in i:
+                    if j.get() == "1":
+                        index1 = list_of_entries.index(i)+1
+                        index2 = i.index(j) + 1
+                        edges.append([index1, index2])
+                        node_list1.append(index1)
+                        node_list2.append(index2)
+            G = nx.DiGraph()
+            pos = nx.spring_layout(G)
+            G.add_edges_from(edges)
+            # nx.draw_networkx_nodes(G, pos, nodelist=node_list1,node_color='r')
+            # nx.draw_networkx_nodes(G, pos, nodelist=node_list2,node_color='b')
+            color_map = []
+            for i in G.nodes():
+                if i in node_list1:
+                    color_map.append("blue")
+                else:
+                    color_map.append("red")
+
+            print(edges)
+
+
+            nx.draw_networkx(G, node_color=color_map,arrowsize=13, font_size=13, width=1.3)
+            plt.show()
+
+
+
+        win2 = Toplevel(root, bg=self.bg)
+        win2.title("Матриця суміжності")
+        frame1 = Frame(win2, bg="#000059")
+        frame1.pack(fill=X, pady=5)
+        header5 = Label(frame1, bg="#00035C", fg='#A9B7C6', font=("Times", 19), text="Матриця суміжності")
+        header5.pack(fill=X)
+
+        frame2 = Frame(win2, bg=self.bg)
+        frame2.pack()
+
+
+
+
+        for i in range(10):
+            l = Label(frame2, bg=self.bg, fg=self.fg, text=i + 1, font=("Arial", 21))
+            l.grid(row=0, column=i + 1)
+        for i in range(10):
+            l = Label(frame2, bg=self.bg, fg=self.fg, text=i + 1, font=("Arial", 21))
+            l.grid(row=i + 1, column=0, sticky=E)
+
+        list_of_entries = list()
+        for row in range(10):
+            list_of_entries.append([])
+            for column in range(10):
+                entry = Entry(frame2,  bg="#00387F", fg='#E8E8E7', width=3, font=("", 21), bd=3, justify=CENTER, relief=FLAT)
+                entry.grid(row=row+1, column=column+1)
+                entry.insert(END, "0")
+                entry.bind('<Button-1>', switch)
+                list_of_entries[row].append(entry)
+        frame3 = Frame(win2, bg=self.bg)
+        frame3.pack()
+
+        b_build_graph = Button(win2, width=17, text="Build Graph", font=("Arial", 12), bg='#FF8C00', activebackground='#284C79', relief=GROOVE, command=build_graph)
+        b_build_graph.pack(pady=10)
+
+
+
+
+
+
+
+
+
 
     def determine_the_variant(self):
         G = 91
